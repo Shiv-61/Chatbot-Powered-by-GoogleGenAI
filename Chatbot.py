90aed98aebb596f1648360d703c95ef3dc53c797
Shiv Gowda <151723225+Shiv-61@users.noreply.github.com>
2024-12-14 22:26:33 +0530

    Add files via upload

---
+++ Chatbot.py
@@ -0,0 +1,15 @@
+import google.generativeai as ai
+
+API_KEY = 'AIzaSyApuwquT3nEv23TJtDEOg6MVllNc9A8pQs'
+ai.configure(api_key=API_KEY)
+
+model = ai.GenerativeModel("gemini-pro")
+chat = model.start_chat()
+
+while True:
+    msg = input("You : ")
+    if msg.lower() == "exit" or msg.lower()=="goodbye":
+        print("ApexAi : GoodBye!")
+        break
+    reponse = chat.send_message(msg)
+    print("ApexAi : ",reponse.text)
